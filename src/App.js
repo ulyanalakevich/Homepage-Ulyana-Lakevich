c5e693a78bca11747b6d045fb7aebbb8d9db631e
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-06-12 11:09:27 +0200

    App js modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navigation from "./common/Navigation";
+import Home from "./features/Home";
+import About from "./features/About";
+import Portfolio from "./features/Portfolio";
+import Contact from "./features/Contact";
+import Themes from "./common/Themes";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navigation/>
+    <Themes/>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
