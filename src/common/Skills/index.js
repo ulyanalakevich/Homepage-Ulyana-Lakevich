1b2f3ebd738e64e28dfc70f7afc63346b464f220
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-06-12 11:12:44 +0200

    Skills added to about

---
+++ src/common/Skills/index.js
@@ -0,0 +1,17 @@
+import { skills } from "../../data";
+
+const Skills = () => {
+  return (
+    <>
+      {skills.map(({title}, index) => {
+        return (
+          <div className="skills__box" key={index}>
+            <h3 className="skills__title">{title}</h3>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Skills;
