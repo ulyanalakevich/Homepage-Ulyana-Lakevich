a134863616e647ae2527b47b473bab1f04b92146
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-06-12 11:12:32 +0200

    Stats added to About

---
+++ src/common/Stats/index.js
@@ -0,0 +1,18 @@
+const Stats = () => {
+  return (
+    <div className="stats__box">
+      <h1 className="stats__title">Junior Frontend Developer</h1>
+      <p className="stats__description">
+        I am a motivated junior frontend developer with a strong desire for
+        personal growth and a passion for creating impactful user experiences. I
+        am highly responsible and possess the ability to quickly learn new
+        technologies and industry trends. My goal is to contribute to your
+        team's success by leveraging my skills in HTML, CSS, and JavaScript.
+        Together, we can deliver exceptional digital experiences that exceed
+        expectations.
+      </p>
+    </div>
+  );
+};
+
+export default Stats;
