f73bc85887bcd5d2062a41159e2a858e7eaedb36
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-06-12 11:11:39 +0200

    About page added

---
+++ src/features/About/index.js
@@ -0,0 +1,74 @@
+import { FaDownload } from "react-icons/fa";
+import Info from "../../common/Info";
+import Stats from "../../common/Stats";
+import CV from "../../assets/profile.jpeg";
+import "./styled.css";
+import Skills from "../../common/Skills";
+import { resume } from "../../data";
+import Resume from "../../common/Resume";
+
+const About = () => {
+  return (
+    <main className="section container">
+      <section className="about">
+        <h2 className="section__title">
+          About<span> Me</span>
+        </h2>
+
+        <div className="about__container grid">
+          <div className="about__info">
+            <h3 className="section__subtitle">Personal Infos</h3>
+
+            <ul className="info__list grid">
+              <Info />
+            </ul>
+            <a href={CV} download="" className="button">
+              Download CV
+              <span className="button__icon">
+                <FaDownload />
+              </span>
+            </a>
+          </div>
+
+          <div className="stats grid">
+            <Stats />
+          </div>
+        </div>
+      </section>
+      <div className="separator"></div>
+
+      <section className="skills">
+        <h3 className="section__subtitle subtitle__center">My Skills</h3>
+        <div className="skills__container grid">
+          <Skills />
+        </div>
+      </section>
+
+      <div className="separator"></div>
+
+      <section className="resume">
+      <h3 className="section__subtitle subtitle__center">Experience & Education</h3>
+     <div className="resume__container grid">
+      <div className="resume__data">
+        {resume.map((val) => {
+          if (val.category === 'experience') {
+            return <Resume key={val.id} {...val} />;
+          }
+        })}
+      </div>
+
+      <div className="resume__data">
+        {resume.map((val) => {
+          if (val.category === 'education') {
+            return <Resume key={val.id} {...val} />;
+          }
+        })}
+      </div>
+     </div>
+     
+      </section>
+    </main>
+  );
+};
+
+export default About;
